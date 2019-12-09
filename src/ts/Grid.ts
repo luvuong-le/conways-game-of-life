667a29503fe10b3bb72a3d9d4afad6fbd9901575
luvuong-le <lu-vuongle@hotmail.com>
2019-12-09 23:22:30 +1100

    Functional without own cell class implemented

--- src/ts/Grid.ts
+++ src/ts/Grid.ts
@@ -2,33 +2,59 @@ import Cell from './Cell';
 
 export default class Grid {
 
-    gridColSize: number;
-    gridRowSize: number;
-    gridCellSize: number;
-    renderingContext: CanvasRenderingContext2D;
+    private _gridColSize: number;
+    private _gridRowSize: number;
+    private _gridCellSize: number;
+    private _renderingContext: CanvasRenderingContext2D;
 
     // Multidimensional Array Board => [cols][rows] / [y][x]
-    grid: Cell[][];
+    private _grid: number[][];
+
+    // Accessors
+    get grid(): number[][] {
+        return this._grid;
+    }
+
+    get columns(): number {
+        return this._gridColSize;
+    }
+
+    get rows(): number {
+        return this._gridRowSize;
+    }
+
+    get cellSize(): number {
+        return this._gridCellSize;
+    }
+
+    get renderingContext(): CanvasRenderingContext2D {
+        return this._renderingContext;
+    }
+
+    // Mutators
+    set grid(newBoard: number[][]) {
+        this._grid = newBoard;
+    }
 
     /**
-     * @param  {CanvasRenderingContext2D} renderingContext
+     * @param {CanvasRenderingContext2D} renderingContext
      */
     constructor(renderingContext: CanvasRenderingContext2D) {
-        this.renderingContext = renderingContext
-        this.gridCellSize = 10;
+        this._renderingContext = renderingContext
+        this._gridCellSize = 10;
 
-        this.gridColSize = renderingContext.canvas.width / this.gridCellSize;
-        this.gridRowSize = renderingContext.canvas.height / this.gridCellSize;
+        this._gridColSize = renderingContext.canvas.width / this._gridCellSize;
+        this._gridRowSize = renderingContext.canvas.height / this._gridCellSize;
 
-        this.grid = new Array(this.gridColSize).fill(null).map(_ => new Array(this.gridRowSize).fill(null).map(_ => new Cell()));
+        this._grid = new Array(this.columns).fill(null).map(_ => new Array(this.rows).fill(null).map(_ => Math.floor(Math.random() * 2)));
     }
     /**
      * @description Get the surrounding neighbours of the current cell
      */
-    getNeighbours = (col: number, row: number) => {
+    getNeighbours = (col: number, row: number): number => {
         let neighbourCount = 0;
 
-        // Get neighbours
+        // Get surrounding neighbours
         for (let y = -1; y <= 1; y++) {
             for (let x = -1; x <= 1; x++) {
                 // Continue if the current neighbour is the cell itself
@@ -41,9 +67,9 @@ export default class Grid {
                 const rowToCheck = row + x;
 
                 // Ignore edges
-                if (colToCheck >= 0 && rowToCheck >= 0 && colToCheck < this.gridColSize && rowToCheck < this.gridRowSize) {
+                if (colToCheck >= 0 && rowToCheck >= 0 && colToCheck < this.columns && rowToCheck < this.rows) {
                     const neighbour = this.grid[colToCheck][rowToCheck];
-                    neighbourCount += neighbour.state;
+                    neighbourCount += neighbour;
                 }
             }
         }
@@ -54,53 +80,53 @@ export default class Grid {
     /**
      * @description Creates the next generation
      */
-    generateNextGen = () => {
+    generateNextGen = (): number[][] => {
         // Placeholder copy for current generation instead of modifying directly
-        const newGen: Cell[][] = this.grid.map(grid => [...grid]);
+        const newGeneration: number[][] = this.grid.map(grid => grid.slice());
 
         // Alter the new generation
-        for (let col = 0; col < this.gridColSize; col++) {
-            for (let row = 0; row < this.gridRowSize; row++) {
+        for (let col = 0; col < this.columns; col++) {
+            for (let row = 0; row < this.rows; row++) {
                 const currentCell = this.grid[col][row];
                 const neighbourCount = this.getNeighbours(col, row);
 
-                if (currentCell.state === 0 && neighbourCount === 3) {
-                    newGen[col][row].state = 1;
-                } else if (currentCell.state === 1 && (neighbourCount < 2 || neighbourCount > 3)) {
-                    newGen[col][row].state = 0;
+                if (currentCell === 0 && neighbourCount === 3) {
+                    newGeneration[col][row] = 1;
+                } else if (currentCell === 1 && (neighbourCount < 2 || neighbourCount > 3)) {
+                    newGeneration[col][row] = 0;
                 } else {
-                    newGen[col][row].state = currentCell.state
+                    newGeneration[col][row] = currentCell
                 }
             }
         }
 
-        return newGen;
+        return newGeneration;
     }
 
     /**
      * @description Renders the board
      */
-    renderBoard = () => {
-        for (let col = 0; col < this.gridColSize; col++) {
-            for (let row = 0; row < this.gridRowSize; row++) {
+    renderBoard = (): void => {
+        for (let col = 0; col < this.columns; col++) {
+            for (let row = 0; row < this.rows; row++) {
                 // Draw Board
                 this.renderingContext.beginPath();
-                this.renderingContext.rect(col * this.gridCellSize, row * this.gridCellSize, this.gridCellSize, this.gridCellSize);
-                this.renderingContext.fillStyle = this.grid[col][row].state ? "#000000" : "#ffffff"
+                this.renderingContext.rect(col * this.cellSize, row * this.cellSize, this.cellSize, this.cellSize);
+                this.renderingContext.fillStyle = this.grid[col][row] ? "#000000" : "#ffffff"
                 this.renderingContext.fill();
-                // this.renderingContext.stroke();
+                this.renderingContext.stroke();
                 this.renderingContext.closePath();
             }
         }
     }
 
-    updateBoard = () => {
+    updateBoard = (): void => {
         this.grid = this.generateNextGen();
         this.renderBoard();
         requestAnimationFrame(this.updateBoard);
     }
 
-    beginGenerations = () => {
+    beginGenerations = (): void => {
         requestAnimationFrame(this.updateBoard);
     }
 }
